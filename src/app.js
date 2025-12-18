222073be38e31d7b31d26a2cb88a55fd305850e2
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-18 20:14:29 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -9,8 +9,22 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://password-resetguvi.netlify.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: (origin, callback) => {
+      // allow server-to-server, Postman, curl
+      if (!origin) return callback(null, true);
+
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://password-resetguvi.netlify.app",
+      ];
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
@@ -18,7 +32,7 @@ app.use(
 // ROUTES
 app.use("/api/auth", router);
 
-// ERROR HANDLER (LAST)
+// ERROR HANDLER (ALWAYS LAST)
 app.use(errorHandler);
 
 export default app;
