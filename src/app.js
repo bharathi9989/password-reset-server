e81584c06dec1e52dcd53603179f9ff5848ab680
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-18 19:59:10 +0530

    fixed

--- src/app.js
+++ src/app.js
@@ -9,15 +9,14 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://password-resetguvi.netlify.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://password-resetguvi.netlify.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
 
 // Routes
 
