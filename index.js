8d7425d13e1d50c8142352a596765748d4e8285c
bharathi9989 <er.velubharathi.s@gmail.com>
2026-01-02 23:01:50 +0530

    comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { connectDB } from "./src/config/db.js"
 import { ENV } from "./src/config/env.js"
 
 
-
+// connected to db
 connectDB()
 app.listen(ENV.PORT, () => {
   console.log(`Application Running on ${ENV.PORT}`)
