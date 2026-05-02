15e6ee1e89dc2afc4c6966efb69c200d1cb143a0
bharathi9989 <er.velubharathi.s@gmail.com>
2026-05-02 05:50:50 +0530

    feat : added a register user controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,37 @@ import bcrypt from "bcryptjs";
 import crypto from "crypto";
 import { ENV } from "../config/env.js";
 
+
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+export const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const exists = await User.findOne({ email });
+  if (exists) return res.status(400).json({ message: "User already exists" });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ email, password: hashed });
+
+  res.status(201).json({ message: "Registered successfully" });
+};
+
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+  res.json({ message: "Login successful" });
+};
+
+
 // FORGOT PASSWORD
 export const forgotPassword = async (req, res) => {
   const { email } = req.body;
