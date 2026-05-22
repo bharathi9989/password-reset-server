1189fc997181554fa7fe659a21a1a281246f5f63
bharathi9989 <er.velubharathi.s@gmail.com>
2026-05-22 20:40:19 +0530

    fixed nodemailer

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,27 +2,33 @@ import nodemailer from "nodemailer";
 import { ENV } from "../config/env.js";
 
 const transporter = nodemailer.createTransport({
-  host: ENV.MAIL_HOST,
-  port: Number(ENV.MAIL_PORT),
+  service: "gmail",
 
   auth: {
-    user: ENV.MAIL_USER,
-    pass: ENV.MAIL_PASS,
+    user: ENV.EMAIL_USER,
+    pass: ENV.EMAIL_PASS,
   },
 });
 
 export const sendResetEmail = async (toEmail, link) => {
   try {
-    console.log("📧 MAILTRAP SMTP START");
+    console.log("📧 MAIL START");
 
     const info = await transporter.sendMail({
-      from: "test@mailtrap.io",
+      from: ENV.EMAIL_USER,
       to: toEmail,
-      subject: "Reset Password",
+      subject: "Password Reset Link",
+
       html: `
         <h2>Password Reset</h2>
-        <p>Click below to reset password</p>
-        <a href="${link}">${link}</a>
+
+        <p>Click below link to reset password</p>
+
+        <a href="${link}">
+          Reset Password
+        </a>
+
+        <p>${link}</p>
       `,
     });
 
